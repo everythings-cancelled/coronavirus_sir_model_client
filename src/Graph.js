81393f064ded9a051207404c4639dd06d36a7558
Shaun Carland <shaun@Shauns-MacBook-Pro-2.local>
2020-03-19 23:42:03 -0400

    create empty graph component

---
+++ src/Graph.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Graph extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { points: props.points }
+    }
+
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Graph;
