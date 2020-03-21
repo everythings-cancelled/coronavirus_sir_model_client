e00a76d06df42e559a7d05773ac2e1bea7e48257
dermatobia <webblewebble@gmail.com>
2020-03-21 11:22:48 -0400

    add more input fields

--- src/Components/GraphForm/GraphForm.js
+++ src/Components/GraphForm/GraphForm.js
@@ -1,30 +1,38 @@
 import React, { useState } from 'react';
 
 const GraphForm = props => {
-  const [country, setCountry] = useState('');
+  const initialValues = {
+    country: 'USA',
+    rateSi: .5,
+    rateRi: .01,
+    eons: 5
+  };
+  const [formVal, setFormVal] = useState(initialValues);
 
-  const handleCountryChange = (e) => {
-    setCountry(e.target.value);
+  const handleValueChange = (key, val) => {
+    const newVals = {...formVal};
+    newVals[key] = val;
+    setFormVal(newVals);
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    let params = {
-      country: country,
-      rateSi: 0.5,
-      rateRi: 0.01,
-      eons: 2
-    };
-
-    props.onSubmit(params);
+    props.onSubmit(formVal);
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="country">
-        Country:
-        <input id="country" type="text" value={country} onChange={handleCountryChange} />
-      </label>
+      <label htmlFor="country">Country: </label>
+      <input id="country" type="text" value={formVal.country} onChange={e => handleValueChange('country', e.target.value)} />
+
+      <label htmlFor="rateSi">Rate Si: </label>
+      <input id="rateSi" type="text" value={formVal.rateSi} onChange={e => handleValueChange('rateSi', e.target.value)} />
+
+      <label htmlFor="rateRi">Rate Ri: </label>
+      <input id="rateRi" type="text" value={formVal.rateRi} onChange={e => handleValueChange('rateRi', e.target.value)} />
+
+      <label htmlFor="eons">Eons: </label>
+      <input id="eons" type="text" value={formVal.eons} onChange={e => handleValueChange('eons', e.target.value)} />
 
       <input type="submit" value="Submit" />
     </form>
