86ace7380af1a2e0bd92cdaba8183a14d56dae46
dermatobia <webblewebble@gmail.com>
2020-03-21 12:47:31 -0400

    update fetch data with useEffect. pass form values to main page

--- src/Components/GraphForm/GraphForm.js
+++ src/Components/GraphForm/GraphForm.js
@@ -7,32 +7,32 @@ const GraphForm = props => {
     rateRi: .01,
     eons: 5
   };
-  const [formVal, setFormVal] = useState(initialValues);
+  const [params, setFormVal] = useState(initialValues);
 
   const handleValueChange = (key, val) => {
-    const newVals = {...formVal};
+    const newVals = {...params};
     newVals[key] = val;
     setFormVal(newVals);
   }
 
-  const handleSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    props.onSubmit(formVal);
+    props.handleSubmit(params);
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={onSubmit}>
       <label htmlFor="country">Country: </label>
-      <input id="country" type="text" value={formVal.country} onChange={e => handleValueChange('country', e.target.value)} />
+      <input id="country" type="text" value={params.country || ''} onChange={e => handleValueChange('country', e.target.value)} />
 
       <label htmlFor="rateSi">Rate Si: </label>
-      <input id="rateSi" type="text" value={formVal.rateSi} onChange={e => handleValueChange('rateSi', e.target.value)} />
+      <input id="rateSi" type="text" value={params.rateSi || ''} onChange={e => handleValueChange('rateSi', e.target.value)} />
 
       <label htmlFor="rateRi">Rate Ri: </label>
-      <input id="rateRi" type="text" value={formVal.rateRi} onChange={e => handleValueChange('rateRi', e.target.value)} />
+      <input id="rateRi" type="text" value={params.rateRi || ''} onChange={e => handleValueChange('rateRi', e.target.value)} />
 
       <label htmlFor="eons">Eons: </label>
-      <input id="eons" type="text" value={formVal.eons} onChange={e => handleValueChange('eons', e.target.value)} />
+      <input id="eons" type="text" value={params.eons || ''} onChange={e => handleValueChange('eons', e.target.value)} />
 
       <input type="submit" value="Submit" />
     </form>
--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,32 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import GraphForm from '../../Components/GraphForm/GraphForm';
 import InfectionChart from '../../Components/InfectionChart/InfectionChart';
 import './Main.scss';
 
 const Main = () => {
   const url = 'https://coronavirus-sir-model-api.herokuapp.com/v1/sir_model';
-  let data = {};
+  const [fetching, setFetching] = useState(false);
+  const [params, setParams] = useState({});
+  const [data, setData] = useState({});
 
-  const fetchData = params => {
-    fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(params)
-    })
-    .then(res => res.json())
-    .then(result => {
-      console.log('Success:', result);
-      data = result;
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-  };
+  useEffect(() => {
+    if (fetching) {
+      fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(params)
+      })
+      .then(res => res.json())
+      .then(result => {
+        setFetching(false);
+        setData({
+          ...result,
+          hospitalCapacity: result.population / 10000 * result.hospitalBedsPer10000People
+        });
+      })
+      .catch((error) => {
+        setFetching(false);
+      });
+    }
+  }, [fetching])
+
+  const handleSubmit = (formValues) => {
+    setFetching(true);
+    setParams(formValues);
+  }
 
   return (
     <div className="Main">
       <h1>Coronavirus SIR</h1>
-      <GraphForm onSubmit={fetchData} />
+      <GraphForm handleSubmit={handleSubmit} />
       <InfectionChart />
+      {data && data.country && (
+        <div>
+        <p>Country: {data.country}</p>
+        <p>Population: {data.population}</p>
+        <p>Hospital Bed available: {data.hospitalCapacity}</p>
+        </div>
+      )}
     </div>
   )
 }
