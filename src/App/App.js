2a8b8a5c6987d1a51a95d23203c24e7b32c6e367
Ergi Laze <exl736@student.bham.ac.uk>
2020-03-22 13:23:01 +0000

    Project structure clean-up

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Main from './Pages/Main/Main';
+import Main from 'Pages/Main/Main';
+import GlobalStyles from 'theme/global';
 
 function App() {
   return (
     <div className="App">
       <Main />
+      <GlobalStyles />
     </div>
   );
 }
